f59dda30f6884fd2efc596733601981299a230e2
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-02-24 21:07:18 +0530

    set up routing for BorrowersComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './page/login/login.component';
 import { ViewAllBooksComponent } from './page/view-all-books/view-all-books.component';
 import { RegisterComponent } from './page/register/register.component';
+import { ViewBorrowersComponent } from './page/view-borrowers/view-borrowers.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path:"sign-up",
         component:RegisterComponent
+    },
+    {
+        path:"view-borrowers",
+        component:ViewBorrowersComponent
     }
 ];
