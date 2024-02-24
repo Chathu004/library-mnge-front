99c8d90dc4f8a76772e1d8ef28d29cbfb06935d1
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-02-24 21:06:14 +0530

    create  component for borrowers

---
+++ src/app/page/view-borrowers/view-borrowers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-borrowers',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-borrowers.component.html',
+  styleUrl: './view-borrowers.component.css'
+})
+export class ViewBorrowersComponent {
+
+}
