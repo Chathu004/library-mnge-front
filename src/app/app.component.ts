b2b73bb81007426ee544417506156876e9689452
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-02-27 10:17:53 +0530

    updated app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './common/nav/nav.component';
 import { ViewAllBooksComponent } from './page/view-all-books/view-all-books.component';
+import { ViewBorrowersComponent } from './page/view-borrowers/view-borrowers.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavComponent,ViewAllBooksComponent],
+  imports: [CommonModule, RouterOutlet, NavComponent,ViewAllBooksComponent,ViewBorrowersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
