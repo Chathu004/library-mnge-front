c97a73a7b849c1bf993a22062bda29e1f610f63d
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-02-29 23:11:33 +0530

    updated users

--- src/app/page/view-borrowers/view-borrowers.component.ts
+++ src/app/page/view-borrowers/view-borrowers.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+
+import { HttpClient,HttpClientModule } from '@angular/common/http';
+import { Component,OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-borrowers',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,FormsModule,CommonModule],
   templateUrl: './view-borrowers.component.html',
   styleUrl: './view-borrowers.component.css'
 })
-export class ViewBorrowersComponent {
+export class ViewBorrowersComponent implements OnInit {
+  private http;
+  public borrowersList : any={};
 
+  constructor(private httpClient : HttpClient){
+    this.http = httpClient;
+  }
+
+
+  ngOnInit(): void {
+    this.loadBorrowers();
+  }
+  loadBorrowers() {
+    this.http.get(`http://localhost:8080/borrowers/get`).subscribe((data)=>{
+      this.borrowersList = data;
+      console.log(this.borrowersList);
+  });
+
+}
 }
