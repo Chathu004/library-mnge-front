15a6a2ba6c96adf2c6cb506828124c5c546b10b5
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-03-01 15:02:05 +0530

    updated borrowers

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import { HttpClient,HttpClientModule } from '@angular/common/http';
 import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -14,6 +15,18 @@ export class RegisterComponent implements OnInit {
   private http;
   public countryList:any;
   public selectedCountry:any;
+  public isExistUser:any;
+  public selectedCountryCode:any
+  public userObj ={
+    firstName:null,
+    lastName:null,
+    userName:null,
+    email:null,
+    address:null,
+    address2:null,
+    country:null,
+    contactNumber:null
+  }
 
   constructor(private httpClient: HttpClient){
     this.http= httpClient;
@@ -26,14 +39,44 @@ export class RegisterComponent implements OnInit {
     let api = "https://restcountries.com/v3.1/all"
     this.http.get(api).subscribe(Response=>{
       this.countryList = Response;
-      console.log(Response);
+      console.log(this.countryList);
       
     })
   }
   setSelectedCountry(country:any){
     this.selectedCountry=country;
+    this.selectedCountryCode=country.idd.root+""+country.idd.suffixes[0];
+    console.log(this.selectedCountryCode);
+    
     console.log(this.selectedCountry);
     
+  }
+  submitForm(){
+    console.log(this.userObj);
+    this.http.get(`http://localhost:8081/borrower/is-exist-userName/${this.userObj.userName}`).subscribe(data=>{
+    console.log(data);
+    this.isExistUser=data;
+    this.registerUser(this.isExistUser);
+    
+    })
+    
+  }
+  registerUser(isExistUser:any){
+    if(!isExistUser==true){
+      this.http.post("http://localhost:8081/borrower/add",this.userObj).subscribe(data=>{
+        Swal.fire({
+          title:"Success",
+          text:`${this.userObj.userName} has been registered !`,
+          icon:"success"
+      })
+      })
+    }else{
+      Swal.fire({
+        title:"Can't Register this User",
+        text:`${this.userObj.userName} has already been registered !`,
+        icon:"error"
+    })
+    }
   }
 
 }
