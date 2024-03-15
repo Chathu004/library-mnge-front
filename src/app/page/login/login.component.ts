3afcbaa8542e6c0707d276e1f1e18c76e1dd7419
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-03-15 23:54:41 +0530

    implementation of login

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,HttpClientModule,CommonModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginObj:any={
+    "email":"",
+    "password":""
+  }
+  constructor(private http:HttpClient,private router:Router){}
+
+  login(){
+    this.http.post("http://localhost:8081/login/request-login",this.loginObj).subscribe((res:any)=>{
+      console.log(res);
+      if(res==true){
+        this.router.navigate(['/view-all-books']);
+      }else{
+        alert("You can't login!")
+      }
+      
+    })
+    
+  }
 
 }
